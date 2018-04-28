f23ddc6fdb5c0e25751ac4ac59aa55ce1dffee53
shivam3746 <25079911+shivam3746@users.noreply.github.com>
2018-04-28 10:18:25 +0530

    Update Question10.java

--- app/src/main/java/com/example/pavlion/quizapp/Question10.java
+++ app/src/main/java/com/example/pavlion/quizapp/Question10.java
@@ -9,19 +9,31 @@ import android.widget.Toast;
 public class Question10 extends AppCompatActivity {
 
     public void onClickobama(View view) {
-        Toast.makeText(this, "Wrong! Try again", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Wrong! Better luck next time", Toast.LENGTH_SHORT).show();
+        rd38.setEnabled(false);
+        rd39.setEnabled(false);
+        rd40.setEnabled(false);
     }
 
     public void onClickbclinton(View view) {
-        Toast.makeText(this, "Wrong! Try again", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Wrong! Better luck next time", Toast.LENGTH_SHORT).show();
+        rd37.setEnabled(false);
+        rd39.setEnabled(false);
+        rd40.setEnabled(false);
     }
 
     public void onClickhclinton(View view) {
-        Toast.makeText(this, "Wrong! Try again", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Wrong! Better luck next time", Toast.LENGTH_SHORT).show();
+        rd37.setEnabled(false);
+        rd38.setEnabled(false);
+        rd40.setEnabled(false);
     }
 
     public void onClicktrump(View view) {
         Toast.makeText(this, "Correct! Well Done", Toast.LENGTH_SHORT).show();
+        rd37.setEnabled(false);
+        rd38.setEnabled(false);
+        rd39.setEnabled(false);
     }
 
 
