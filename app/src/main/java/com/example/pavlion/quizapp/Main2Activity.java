6709ae38d0303902f474a2107934a82da7cb7f45
shivam3746 <25079911+shivam3746@users.noreply.github.com>
2018-04-28 11:34:09 +0530

    Update Main2Activity.java

--- app/src/main/java/com/example/pavlion/quizapp/Main2Activity.java
+++ app/src/main/java/com/example/pavlion/quizapp/Main2Activity.java
@@ -7,21 +7,38 @@ import android.view.View;
 import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
+    
+    Button btn5 = (Button) findViewById(R.id.rd5);
+    Button btn6 = (Button) findViewById(R.id.rd6);
+    Button btn7 = (Button) findViewById(R.id.rd7);
+    Button btn8 = (Button) findViewById(R.id.rd8);
 
     public void onClick1994(View view) {
-        Toast.makeText(this, "Wrong! Try again", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Wrong! Better luck next time", Toast.LENGTH_SHORT).show();
+        btn6.setEnabled(false);
+        btn7.setEnabled(false);
+        btn8.setEnabled(false);
     }
 
     public void onClick1956(View view) {
-        Toast.makeText(this, "Wrong! Try again", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Wrong! Better luck next time", Toast.LENGTH_SHORT).show();
+        btn5.setEnabled(false);
+        btn7.setEnabled(false);
+        btn8.setEnabled(false);
     }
 
     public void onClick2006(View view) {
-        Toast.makeText(this, "Wrong! Try again", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Wrong! Better luck next time", Toast.LENGTH_SHORT).show();
+        btn5.setEnabled(false);
+        btn6.setEnabled(false);
+        btn8.setEnabled(false);
     }
 
     public void onClick1998(View view) {
         Toast.makeText(this, "Correct! Well Done", Toast.LENGTH_SHORT).show();
+        btn5.setEnabled(false);
+        btn6.setEnabled(false);
+        btn7.setEnabled(false);
     }
 
     @Override
