7a2628d0249a9780685731ca9e90a15a33d93a7d
shivam3746 <25079911+shivam3746@users.noreply.github.com>
2018-04-28 10:30:09 +0530

    Update Question7.java

--- app/src/main/java/com/example/pavlion/quizapp/Question7.java
+++ app/src/main/java/com/example/pavlion/quizapp/Question7.java
@@ -9,19 +9,31 @@ import android.widget.Toast;
 public class Question7 extends AppCompatActivity {
 
     public void onClickrugby(View view) {
-        Toast.makeText(this, "Wrong! Try again", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Wrong! Better luck next time", Toast.LENGTH_SHORT).show();
+        rd26.setEnabled(false);
+        rd27.setEnabled(false);
+        rd28.setEnabled(false);
     }
 
     public void onClickicehockey(View view) {
         Toast.makeText(this, "Correct! Well Done", Toast.LENGTH_SHORT).show();
+        rd25.setEnabled(false);
+        rd27.setEnabled(false);
+        rd28.setEnabled(false);
     }
 
     public void onClickfootball(View view) {
-        Toast.makeText(this, "Wrong! Try again", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Wrong! Better luck next time", Toast.LENGTH_SHORT).show();
+        rd25.setEnabled(false);
+        rd26.setEnabled(false);
+        rd28.setEnabled(false);
     }
 
     public void onClickcricket(View view) {
-        Toast.makeText(this, "Wrong! Try again", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Wrong! Better luck next time", Toast.LENGTH_SHORT).show();
+        rd25.setEnabled(false);
+        rd26.setEnabled(false);
+        rd27.setEnabled(false);
     }
 
 
