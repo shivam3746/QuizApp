f271aaf786f485d6701d5b3fc56c4c9ee1d458ec
shivam3746 <25079911+shivam3746@users.noreply.github.com>
2018-04-28 11:23:27 +0530

    Update Question4.java

--- app/src/main/java/com/example/pavlion/quizapp/Question4.java
+++ app/src/main/java/com/example/pavlion/quizapp/Question4.java
@@ -7,22 +7,39 @@ import android.view.View;
 import android.widget.Toast;
 
 public class Question4 extends AppCompatActivity {
+    
+    Button btn13 = (Button) findViewById(R.id.rd13);
+    Button btn14 = (Button) findViewById(R.id.rd14);
+    Button btn15 = (Button) findViewById(R.id.rd15);
+    Button btn16 = (Button) findViewById(R.id.rd16);
 
 
     public void onClickrk(View view) {
         Toast.makeText(this, "Wrong! Try again", Toast.LENGTH_SHORT).show();
+        btn14.setEnabled(false);
+        btn15.setEnabled(false);
+        btn16.setEnabled(false);
     }
 
     public void onClickjr(View view) {
         Toast.makeText(this, "Wrong! Try again", Toast.LENGTH_SHORT).show();
+        btn13.setEnabled(false);
+        btn15.setEnabled(false);
+        btn16.setEnabled(false);
     }
 
     public void onClickym(View view) {
         Toast.makeText(this, "Correct! Well Done", Toast.LENGTH_SHORT).show();
+        btn13.setEnabled(false);
+        btn14.setEnabled(false);
+        btn16.setEnabled(false);
     }
 
     public void onClicklt(View view) {
         Toast.makeText(this, "Wrong! Try again", Toast.LENGTH_SHORT).show();
+        btn13.setEnabled(false);
+        btn14.setEnabled(false);
+        btn15.setEnabled(false);
     }
 
 
